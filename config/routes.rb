a2c86d888571763cbd39ece39e8cc8bd14f80a9f
byVasko <vasyl101710@gmail.com>
2019-02-05 18:43:55 +0200

    adding the ability to create, edit and delete posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  root 'pages#index'
+
+  resources :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
